af50ecdf4677fd87e3dd08a16d5184cbe5df0bc2
DrSkyFox <52832487+DrSkyFox@users.noreply.github.com>
2021-04-06 01:09:46 +0300

    3

--- shop-database/src/main/java/ru/geekbrains/ShopPictureServiceApplication.java
+++ shop-database/src/main/java/ru/geekbrains/ShopPictureServiceApplication.java
@@ -1,4 +1,4 @@
-package ru.geekbrains.shoppictureservice;
+package ru.geekbrains;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ShopPictureServiceApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ShopPictureServiceApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(ShopPictureServiceApplication.class, args);
+	}
 
 }
