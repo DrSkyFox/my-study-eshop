731288c5a17adc48a11a445b657b973b346ae1bb
DrSkyFox <52832487+DrSkyFox@users.noreply.github.com>
2021-04-21 21:53:13 +0300

    loging aut

---
+++ shop-ui/src/main/java/ru/geekbrains/controller/LoginController.java
@@ -0,0 +1,18 @@
+package ru.geekbrains.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+
+@Controller
+
+public class LoginController {
+
+
+    private static final Logger logger = LoggerFactory.getLogger(LoginController.class.getName());
+
+
+
+
+
+}
