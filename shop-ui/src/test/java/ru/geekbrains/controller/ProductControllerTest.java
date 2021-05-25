b15c7867ca631b15ca91d0c6388dd30a370931f4
DrSkyFox <52832487+DrSkyFox@users.noreply.github.com>
2021-05-26 02:40:43 +0300

    add test

---
+++ shop-ui/src/test/java/ru/geekbrains/controller/ProductControllerTest.java
@@ -0,0 +1,72 @@
+package ru.geekbrains.controller;
+
+import org.hamcrest.BaseMatcher;
+import org.hamcrest.Description;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+
+import ru.geekbrains.persist.model.Brand;
+import ru.geekbrains.persist.model.Category;
+import ru.geekbrains.persist.model.Product;
+import ru.geekbrains.persist.repositories.BrandRepository;
+import ru.geekbrains.persist.repositories.CategoryRepository;
+import ru.geekbrains.persist.repositories.ProductRepository;
+import ru.geekbrains.replication.ProductRepr;
+
+import java.math.BigDecimal;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+
+@ActiveProfiles("test")
+@TestPropertySource(locations = "classpath:application-test.properties")
+@AutoConfigureMockMvc
+@SpringBootTest
+public class ProductControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Autowired
+    private BrandRepository brandRepository;
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Test
+    public void testProductDetails() throws Exception {
+        Brand brand = brandRepository.save(new Brand("brand"));
+        Category category = categoryRepository.save(new Category("Category"));
+        Product product = productRepository.save(new Product("Product", new BigDecimal(1234), category, brand));
+
+        mvc.perform(get("/product/" + product.getId()))
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(view().name("product-details"))
+                .andExpect(model().attributeExists("product"))
+                .andExpect(model().attribute("product", new BaseMatcher<Product>() {
+
+                    @Override
+                    public void describeTo(Description description) {
+
+                    }
+
+                    @Override
+                    public boolean matches(Object o) {
+                        if (o instanceof ProductRepr) {
+                            ProductRepr productRepr = (ProductRepr) o;
+                            return productRepr.getId().equals(product.getId());
+                        }
+                        return false;
+                    }
+                }));
+    }
+}
