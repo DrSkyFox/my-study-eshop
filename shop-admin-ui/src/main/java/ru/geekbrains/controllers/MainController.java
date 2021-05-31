bbaf593609197b51a708d3053762758d5cf3d5ec
DrSkyFox <52832487+DrSkyFox@users.noreply.github.com>
2021-05-31 16:22:44 +0300

    add logger

--- shop-admin-ui/src/main/java/ru/geekbrains/controllers/MainController.java
+++ shop-admin-ui/src/main/java/ru/geekbrains/controllers/MainController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class MainController {
 
+    
+
     @RequestMapping("/")
     public String indexPage(Model model) {
         model.addAttribute("activePage", "None");
