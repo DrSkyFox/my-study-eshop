8b2ffab58ad0c3aea1fa8f40e86165f713476ec7
DrSkyFox <52832487+DrSkyFox@users.noreply.github.com>
2021-06-01 23:21:12 +0300

    order and personal lk

---
+++ shop-ui/src/main/java/ru/geekbrains/serviceimplementation/OrderServiceImpl.java
@@ -0,0 +1,8 @@
+package ru.geekbrains.serviceimplementation;
+
+import org.springframework.stereotype.Service;
+import ru.geekbrains.service.OrderService;
+
+@Service
+public class OrderServiceImpl implements OrderService {
+}
